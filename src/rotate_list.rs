8d897ec959e5586adead0c53d13c829aee20b30b
belltoy <belltoy@gmail.com>
2020-12-06 15:38:10 +0800

    update rotate_list

--- src/rotate_list.rs
+++ src/rotate_list.rs
@@ -45,10 +45,19 @@ impl ListNode {
 }
 
 pub struct Solution;
+
 impl Solution {
+
+    /// 双指针，两个 mut 所以需要用到 unsafe
     pub fn rotate_right(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+        debug_assert!(k >= 0);
+
         if head.is_none() {
             return None;
+        }
+
+        if k == 0 {
+            return head;
         }
 
         let mut len = 0;
@@ -58,10 +67,14 @@ impl Solution {
             ptr = &node.next;
         }
 
+        let step = k % len;
+        if step == 0 {
+            return head;
+        }
+
+
         let mut fast: *mut _ = &mut head;
         let mut slow: *mut _ = fast;
-        let step = k % len;
-
         unsafe {
             // fast pointer go first
             for _ in 0..step {
@@ -78,5 +91,46 @@ impl Solution {
             let new_head = (*slow).as_mut().unwrap().next.take();
             new_head
         }
+    }
+
+    /// 单指针（引用），安全版本
+    pub fn rotate_right_safe(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+        debug_assert!(k >= 0);
+
+        if head.is_none() {
+            return None;
+        }
+
+        if k == 0 {
+            return head;
+        }
+
+        let mut len = 0;
+        let mut ptr = &head;
+        while let Some(ref node) = ptr {
+            len += 1;
+            ptr = &node.next;
+        }
+
+        let step = k % len;
+
+        if step == 0 {
+            return head;
+        }
+
+        let mut ptr = &mut head;
+        for _ in 1..(len - step) {
+            ptr = &mut ptr.as_mut().unwrap().next;
+        }
+
+        let mut new_head = ptr.as_mut().unwrap().next.take();
+        let mut ptr = &mut new_head;
+        while ptr.is_some() && ptr.as_ref().unwrap().next.is_some() {
+            ptr = &mut ptr.as_mut().unwrap().next;
+        }
+
+        ptr.as_mut().unwrap().next = head;
+
+        new_head
     }
 }
